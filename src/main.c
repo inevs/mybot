aed6b8f1d30e2b3e9766c8761610121f7318b97a
Sven Günther <sven.guenther@it-agile.de>
2020-05-15 08:32:59 +0200

    speed control by remote control

--- src/main.c
+++ src/main.c
@@ -17,15 +17,46 @@ void setup() {
 
 int main(void) {
 
+    int8_t speedInc = 10;
+    int8_t leftSpeed = 0;
+    int8_t rightSpeed = 0;
+    uint16_t command = 0x0;
+    bool toggle = 0;
+
     setup();
     sei();
-    setSpeed(0, 0);
+    setSpeed(leftSpeed, rightSpeed);
 
     while(1) {
-       lcdSetCursor(0, 0); lcdPrintf("Command = %d", getRC5Cmd());
-       lcdSetCursor(1, 0); lcdPrintf("Address = %d", getRC5Addr());
-       lcdSetCursor(2, 0); lcdPrintf("rc5toggle = %d", getRC5Toggle());
-       delay(1000);
+        command = getRC5Cmd();
+        lcdSetCursor(0, 0); lcdPrintf("Command = %d", command);
+        lcdSetCursor(1, 0); lcdPrintf("Address = %d", getRC5Addr());
+        lcdSetCursor(2, 0); lcdPrintf("rc5toggle = %d", getRC5Toggle());
+        lcdSetCursor(3, 0); lcdPrintf("l = %d r = %d", leftSpeed, rightSpeed);
+        delay(1000);
+
+        if (toggle != getRC5Toggle()) {
+            toggle = getRC5Toggle();
+            if (command == 21) {
+                leftSpeed += speedInc;
+                rightSpeed += speedInc;
+            }
+            if (command == 20) {
+                leftSpeed -= speedInc;
+                rightSpeed -= speedInc;
+            }
+            if (command == 16) {
+                rightSpeed += speedInc;
+            }
+            if (command == 17) {
+                leftSpeed += speedInc;
+            }
+            if (command == 17) {
+                leftSpeed = 0;
+                rightSpeed = 0;
+            }
+            setSpeed(leftSpeed, rightSpeed);
+        }
     }
     
     return 0;
